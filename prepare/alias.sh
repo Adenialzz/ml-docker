274109f42647abf4afd375476c0c9b7525f02283
Adenialzz <1160180309@qq.com>
2023-05-07 23:54:21 +0800

    add make alias

--- prepare/alias.sh
+++ prepare/alias.sh
@@ -4,13 +4,14 @@ alias lt="ls -lrt"
 alias JJ="conda activate JJ_env"
 alias py="python"
 alias vi="nvim"
-alias sc="source /root/.zshrc"
+alias ss="source /root/.zshrc"
 alias pips-thu="pip install -i https://pypi.tuna.tsinghua.edu.cn/simple"
 alias hss="history | grep"
 
 if [ `uname -s` = Darwin ]; then
 	alias pss="ps -ax | grep -v "grep" | grep -v "pss" | grep --color=auto"
-elif [ `uname -s` = Linux ]; then
+	alias make="make -j`sysctl -n machdep.cpu.core_count`"
+elif [ `uname -s` = Linux]; then
 	alias pss="ps -aux | grep -v "grep" | grep -v "pss" | grep --color=auto"
+	alias make="make -j`nproc`"
 fi
-
